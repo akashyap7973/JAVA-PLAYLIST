148a82aa2f3b90f6db3eb077baf49d610f12b547
akashyap7973 <anukashyap7696@gmail.com>
2026-08-03 16:27:55 +0530

    Add Day 3 Java datatype program

---
+++ Day1/Day2 variable&datatypes/variable.java
@@ -0,0 +1,18 @@
+public class variable  {
+    public static void main(String[] args){
+        int time = 10; 
+        float pi = 3.1466868f;
+        char c = 'A';
+        boolean b = true ;
+        double salary = 1000.50000;
+
+        System.out.println("Interger value :" + time);
+        System.out.println("float value :" +pi);
+        System.out.println("charachter :"+c);
+        System.out.println("boolean value :" +b);
+        System.out.println("double value :" +salary);
+
+
+
+    }
+}

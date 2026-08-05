75c38d400265bab2804185442cf578a5f4581a00
akashyap7973 <anukashyap7696@gmail.com>
2026-08-05 11:13:32 +0530

    commit

---
+++ datatype.java
@@ -0,0 +1,29 @@
+public class datatype{
+    public static void main (String[] args){
+         //interger - byte , short , int , long //
+         byte x = 5 ; 
+         short y = 10 ;
+         int z = 500000;
+         long l = 6000000;
+
+         //*float and double */
+         float f = 10.54f;
+         double d = 23.0988;
+
+        //Character//
+        char X = 'a';
+
+        //boolean value //
+        boolean bool = false;
+
+        System.out.println("Byte value :" +x);
+        System.out.println("Short value :" +y);
+        System.out.println("int  value :" +z);
+        System.out.println("Long value :" +l);
+        System.out.println("Floating value :" +f);
+        System.out.println("Douvle value :" +d);
+        System.out.println("charachter value :" +X);
+        System.out.println("Boolean value :" +bool);
+         
+    }
+}

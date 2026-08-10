251881236db5b6ba158b115c1e4c199c212cc54d
akashyap7973 <anukashyap7696@gmail.com>
2026-08-10 12:36:11 +0530

    Type conversion in java

---
+++ demo.java
@@ -0,0 +1,9 @@
+ public class demo {
+    public static void main(String[]args){
+        float f = 10.54f;
+        float g = 23.0988f;
+
+        System.out.println("Floating value :" +f);
+        System.out.printf("Formatted value: %.20g%n", g);//formatted value means to print the value in a specific format, in this case, with 20 decimal places
+    }
+ }
---
+++ typeConversion.java
@@ -0,0 +1,44 @@
+public class typeConversion {
+    public static void main(String[]args){
+        //implicit conversion (where destination data type should be wider (higer)then source DESTINATION > SOURCE)
+
+        // byte to int //
+        byte b = 12;
+        int i  ;
+        i = b ;
+        System.out.println("Value of i :" +i);
+
+        //Explicit conversion (narrowing conversion) with the use of casting (where destination < source )
+
+        //int to byte //
+         int h = 300;
+         byte B ;
+        //  B = h ; not converted becuse int > byte 
+        B = (byte)h ; // casting 
+
+        char c = 'a';
+        int x ;
+
+        x = (int)c;
+
+        System.out.println("Value of B :" +B);
+        //Outputt is 44 data is trancating use casting byte size is 8 bit that we use only 8 bits //
+        System.out.println("Value of x :" +x);
+
+        //Trancating conversion //
+
+        //float to int //
+
+        float F = 167.88f;
+        int U ;
+         
+        // U = F ; Not allowded because int print only intger not decimal and char value //
+
+        U = (int)F;
+
+        System.out.println("Vlaue of U (int) :" +U);
+
+    
+
+    }
+}
